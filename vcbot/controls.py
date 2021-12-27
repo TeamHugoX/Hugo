6176dd45736e1115acf36d13a0b703181100a188
OMG <93146537+OMGHelo@users.noreply.github.com>
2021-12-27 09:22:52 +0700

    `Lol`

---
+++ vcbot/controls.py
@@ -0,0 +1,116 @@
+# Hugo - UserBot
+# Copyright (C) TeamHugoX
+#
+# This file is a part of < https://github.com/TeamHugoX/Hugo/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamHugoX/Hugo/blob/main/LICENSE/>.
+
+"""
+**༄** Commands Available -
+
+• `{i}joinvc <optional chat id/username>`
+   Join the voice chat.
+
+• `{i}leavevc`
+   Leave the voice chat.
+
+• `{i}rejoin`
+   Re-join the voice chat, incase of errors.
+
+• `{i}volume <number>`
+   Put number between 1 to 100
+"""
+
+from pytgcalls.exceptions import NotConnectedError
+
+from . import *
+
+
+@vc_asst("joinvc")
+async def join_(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        if not chat.startswith("@"):
+            chat = int(chat)
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, get_string("vcbot_2").format(str(e)))
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat, event)
+    if not ultSongs.group_call.is_connected:
+        await ultSongs.vc_joiner()
+
+
+@vc_asst("(leavevc|stopvc)")
+async def leaver(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        if not chat.startswith("@"):
+            chat = int(chat)
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, get_string("vcbot_2").format(str(e)))
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    await ultSongs.group_call.stop()
+    if CLIENTS.get(chat):
+        del CLIENTS[chat]
+    if VIDEO_ON.get(chat):
+        del VIDEO_ON[chat]
+    await eor(event, get_string('vcbot_1'))
+
+
+@vc_asst("rejoin")
+async def rejoiner(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        if not chat.startswith("@"):
+            chat = int(chat)
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, get_string("vcbot_2").format(str(e)))
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    try:
+        await ultSongs.group_call.reconnect()
+    except NotConnectedError:
+        return await eor(event, get_string('vcbot_6'))
+    await eor(event, get_string('vcbot_5'))
+
+
+@vc_asst("volume")
+async def volume_setter(event):
+    if len(event.text.split()) <= 1:
+        return await eor(event, get_string('vcbot_4'))
+    inp = event.text.split()
+    if inp[1].startswith("@"):
+        chat = inp[1]
+        vol = int(inp[2])
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, get_string("vcbot_2").format(str(e)))
+    elif inp[1].startswith("-"):
+        chat = int(inp[1])
+        vol = int(inp[2])
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, get_string("vcbot_2").format(str(e)))
+    elif inp[1].isdigit() and len(inp) == 2:
+        vol = int(inp[1])
+        chat = event.chat_id
+    if vol:
+        ultSongs = Player(chat)
+        await ultSongs.group_call.set_my_volume(int(vol))
+        if vol > 200:
+            vol = 200
+        elif vol < 1:
+            vol = 1
+        return await eor(event, get_string('vcbot_3').format(vol))
---
+++ vcbot/ytplaylist.py
@@ -0,0 +1,61 @@
+# Hugo - UserBot
+# Copyright (C) TeamHugoX
+#
+# This file is a part of < https://github.com/TeamHugoX/Hugo/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamHugoX/Hugo/blob/main/LICENSE/>.
+
+"""
+**༄** Commands Available -
+
+`{i}ytplaylist <playlist link>`
+  play whole playlist in voice chat
+
+"""
+
+from . import *
+
+
+@vc_asst("ytplaylist")
+async def live_stream(e):
+    xx = await eor(e, get_string("com_1"))
+    if len(e.text.split()) <= 1:
+        return await eor(xx, "Are You Kidding Me?\nWhat to Play?")
+    input = e.text.split()
+    if input[1].startswith("-"):
+        chat = int(input[1])
+        song = e.text.split(maxsplit=2)[2]
+    elif input[1].startswith("@"):
+        cid_moosa = (await vcClient.get_entity(input[1])).id
+        chat = int("-100" + str(cid_moosa))
+        song = e.text.split(maxsplit=2)[2]
+    else:
+        song = e.text.split(maxsplit=1)[1]
+        chat = e.chat_id
+    if not (re.search("youtu", song) and re.search("playlist\\?list", song)):
+        return await eor(xx, get_string('vcbot_8'))
+    if not is_url_ok(song):
+        return await eor(xx, '`Only Youtube Playlist please.`')
+    await xx.edit(get_string('vcbot_7'))
+    file, thumb, title, link, duration = await dl_playlist(chat, html_mention(e), song)
+    ultSongs = Player(chat, e)
+    if not ultSongs.group_call.is_connected:
+        if not (await ultSongs.vc_joiner()):
+            return
+        from_user = inline_mention(e.sender)
+        await xx.reply(
+            "🎸 **Now playing:** [{}]({})\n⏰ **Duration:** `{}`\n👥 **Chat:** `{}`\n🙋‍♂ **Requested by:** {}".format(
+                title[:30] + "...", link, duration, chat, from_user
+            ),
+            file=thumb,
+            link_preview=False,
+        )
+        await xx.delete()
+        await ultSongs.group_call.start_audio(file)
+    else:
+        from_user = html_mention(e)
+        add_to_queue(chat, file, title, link, thumb, from_user, duration)
+        return await eor(
+            xx,
+            f"▶ Added 🎵 **[{title}]({link})** to queue at #{list(VC_QUEUE[chat].keys())[-1]}.",
+        )
