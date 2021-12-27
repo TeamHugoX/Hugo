cc848af06d0726a81ca6e6fa54732d2a83032d08
OMG <93146537+OMGHelo@users.noreply.github.com>
2021-12-27 09:38:03 +0700

    `Added`

---
+++ resources/startup/startup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Hugo - UserBot
+# Copyright (C) TeamHugoX
+#
+# This file is a part of < https://github.com/TeamHugoX/Hugo/ >
+# PLease read the GNU Affero General Public License in <https://www.github.com/TeamHugoX/Hugo/blob/main/LICENSE/>.
+
+
+echo "
+            ──────────────
+            ╔╗╔╗╔╦╗╔══╗╔═╗
+            ║╚╝║║║║║╔═╣║║║
+            ║╔╗║║║║║╚╗║║║║
+            ╚╝╚╝╚═╝╚══╝╚═╝
+            ──────────────
+
+      Visit @HugoProject for updates!!
+
+"
+
+python3 -m pyHugo
