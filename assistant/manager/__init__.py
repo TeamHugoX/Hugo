a20c1dd95cbb464319aa2d9b473364b160e4e5f5
OMG <93146537+OMGHelo@users.noreply.github.com>
2021-12-27 09:55:44 +0700

    `Fixes`

---
+++ assistant/manager/__init__.py
@@ -0,0 +1,10 @@
+# Hugo - UserBot
+# Copyright (C) TeamHugoX
+#
+# This file is a part of < https://github.com/TeamHugoX/Hugo/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamHugoX/Hugo/blob/main/LICENSE/>.
+
+from .. import *
+from pyHugo.misc._decorators import hugo_cmd
+from pyHugo.functions.helper import inline_mention
